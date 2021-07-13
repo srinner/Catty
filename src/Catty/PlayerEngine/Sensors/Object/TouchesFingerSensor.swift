ebd4f49d9f5b65092e6c097d7da6f54cbfa6e828
Stefan Rinner <stefan.rinner@student.tugraz.at>
2021-07-13 17:13:12 +0200

    Calculated touch position within object range

--- src/Catty/PlayerEngine/Sensors/Object/TouchesFingerSensor.swift
+++ src/Catty/PlayerEngine/Sensors/Object/TouchesFingerSensor.swift
@@ -25,6 +25,7 @@
     @objc static let tag = "COLLIDES_WITH_FINGER"
     static let name = kUIFESensorTouchesFinger
     static let defaultRawValue = 0.0
+    static let defaultRawValuePoint = CGPoint(x: 0, y: 0)
     static let requiredResource = ResourceType.touchHandler
     static let position = 20
     static let screenWidth = 500
@@ -46,12 +47,35 @@
 
     func convertToStandardized(rawValue: Double, for spriteObject: SpriteObject) -> Double {
         guard let _ = getTouchManager()?.lastPositionInScene() else { return type(of: self).defaultRawValue }
-        let width = spriteObject.dimensions(of: spriteObject.spriteNode.currentLook).width
-        let height = spriteObject.dimensions(of: spriteObject.spriteNode.currentLook).height
-        return rawValue - Double(TouchesFingerSensor.screenWidth) / 2.0
+        let width = spriteObject.dimensions(of: spriteObject.spriteNode.currentLook).width / 2
+        let height = spriteObject.dimensions(of: spriteObject.spriteNode.currentLook).height / 2
+
+        var position = getTouchPosition()
+        var pointX = PositionXSensor.convertToStandardized(rawValue: Double(position.x), for: spriteObject)
+        var pointY = PositionYSensor.convertToStandardized(rawValue: Double(position.y), for: spriteObject)
+        let touchPoint = CGPoint(x: pointX, y: pointY)
+        position = spriteObject.spriteNode.position
+        pointX = PositionXSensor.convertToStandardized(rawValue: Double(position.x), for: spriteObject)
+        pointY = PositionYSensor.convertToStandardized(rawValue: Double(position.y), for: spriteObject)
+        let objectPoint = CGPoint(x: pointX, y: pointY)
+
+        if (objectPoint.x + width) < touchPoint.x || (objectPoint.x - width) > touchPoint.x {
+            return 0
+        }
+
+        if (objectPoint.y + height) < touchPoint.y || (objectPoint.y - height) > touchPoint.y {
+            return 0
+        }
+        return 1
+        //return rawValue - Double(TouchesFingerSensor.screenWidth) / 2.0
     }
 
     func formulaEditorSections(for spriteObject: SpriteObject) -> [FormulaEditorSection] {
         [.object(position: type(of: self).position, subsection: .motion)]
+    }
+
+    func getTouchPosition() -> CGPoint {
+        guard let lastPosition = getTouchManager()?.lastPositionInScene() else { return type(of: self).defaultRawValuePoint }
+        return lastPosition
     }
 }
